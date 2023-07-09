59a9f3c7073521d602ae3630e00727abf856a2c6
Alexander <alexgomezgomez15@gmail.com>
2023-07-09 14:34:06 -0400

    Excited Kittens answered

--- script.js
+++ script.js
@@ -117,3 +117,19 @@ whereIsWaldo[1][2] = "No One";
 console.log(whereIsWaldo);
 
 console.log(whereIsWaldo[2][1][1]);
+
+//========Excited Kitten ========================
+
+const kittyTalk = ["...human...why you taking pictures of me?...", "...the catnip made me do it...","...why does the red dot always get away..."];
+
+let meow = 0;
+
+
+for(let i = 1; i <= 20; i++){
+    meow = Math.round(Math.random() * (kittyTalk.length - 1));
+    if(i % 2 === 0){
+        console.log(kittyTalk[meow]);
+    }else{
+        console.log("Love me, pet me! HSSSSSS!");
+    }
+}
