931f33a5f64c3dcf4e8143fda3b7c5571a8d52fa
Alexander <alexgomezgomez15@gmail.com>
2023-07-09 13:52:50 -0400

    Fizz Buzz answered

--- script.js
+++ script.js
@@ -4,6 +4,19 @@
 // }
 
 //======Get Even===============
-for(let i = 0; i <=200; i+= 2) {
-    console.log(i);
+// for(let i = 0; i <=200; i+= 2) {
+//     console.log(i);
+// }
+
+//======Fizz Buzz =============
+for (let i = 1; i <=100; i++) {
+    if (i % 3 === 0 && i % 5 ===0) {
+        console.log("FizzBuzz");
+    }else if (i % 3 === 0) {
+        console.log("Fizz");
+    }else if (i % 5 === 0){
+        console.log("Buzz");
+    }else {
+        console.log(i);
+    }
 }
